d3666bc541464d2e0708c84867ebd7eb3d33126d
hammadx02 <hammadaali02@gmail.com>
2024-07-29 21:51:20 +0500

    updated

--- lib/feature/prompt/bloc/prompt_event.dart
+++ lib/feature/prompt/bloc/prompt_event.dart
@@ -3,7 +3,9 @@ part of 'prompt_bloc.dart';
 @immutable
 sealed class PromptEvent {}
 
-class PromptEnteredEvent extends PromptEvent{
+class PromptInitialEvent extends PromptEvent {}
+
+class PromptEnteredEvent extends PromptEvent {
   final String prompt;
 
   PromptEnteredEvent({required this.prompt});
