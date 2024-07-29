e3d030a7902802c8cf4a8ab8a6a9297af1c29b5a
hammadx02 <hammadaali02@gmail.com>
2024-07-29 15:04:07 +0500

    ui updated

--- lib/feature/prompt/ui/create_prompt_screen.dart
+++ lib/feature/prompt/ui/create_prompt_screen.dart
@@ -8,12 +8,73 @@ class CreatePromptScreen extends StatefulWidget {
 }
 
 class _CreatePromptScreenState extends State<CreatePromptScreen> {
+  TextEditingController controller = TextEditingController();
+
+  @override
+  void dispose() {
+    super.dispose();
+    controller.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text(
           "Generate Images 🚀",
+        ),
+      ),
+      body: Container(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Expanded(
+              child: Container(
+                width: double.maxFinite,
+              ),
+            ),
+            Container(
+              height: 240,
+              padding: const EdgeInsets.all(24),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  const Text(
+                    "Enter your prompt",
+                    style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+                  ),
+                  const SizedBox(height: 20),
+                  TextField(
+                    controller: controller,
+                    cursorColor: Colors.deepPurple,
+                    decoration: InputDecoration(
+                      focusedBorder: OutlineInputBorder(
+                        borderSide: const BorderSide(color: Colors.deepPurple),
+                        borderRadius: BorderRadius.circular(12),
+                      ),
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(12),
+                      ),
+                    ),
+                  ),
+                  const SizedBox(height: 20),
+                  SizedBox(
+                    height: 48,
+                    width: double.maxFinite,
+                    child: ElevatedButton.icon(
+                      style: ButtonStyle(
+                        backgroundColor:
+                            WidgetStateProperty.all(Colors.deepPurple),
+                      ),
+                      onPressed: () {},
+                      icon: const Icon(Icons.generating_tokens),
+                      label: const Text("Generate"),
+                    ),
+                  )
+                ],
+              ),
+            )
+          ],
         ),
       ),
     );
