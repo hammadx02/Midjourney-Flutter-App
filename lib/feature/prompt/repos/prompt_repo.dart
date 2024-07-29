d070a68327bec6793903e8c39f6fd50707c03229
hammadx02 <hammadaali02@gmail.com>
2024-07-29 21:36:57 +0500

    updated

--- lib/feature/prompt/repos/prompt_repo.dart
+++ lib/feature/prompt/repos/prompt_repo.dart
@@ -1,27 +1,34 @@
+import 'dart:io';
+
 import 'package:dio/dio.dart';
 
 class PromptRepo {
   static generateImage(String prompt) async {
-
     String url = 'https://api.vyro.ai/v1/imagine/api/generations';
 
     Map<String, dynamic> headers = {
-  'Authorization': 'Bearer vk-***************'
-};
+      'Authorization': 'Bearer vk-***************'
+    };
 
-Map<String, dynamic> payload = {
-        'prompt': prompt,
-        'style_id': '122',
-        'aspect_ratio': '1:1',
-        'cfg': '5',
-        'seed': '1',
-        'high_res_results': '1'
-      };
+    Map<String, dynamic> payload = {
+      'prompt': prompt,
+      'style_id': '122',
+      'aspect_ratio': '1:1',
+      'cfg': '5',
+      'seed': '1',
+      'high_res_results': '1'
+    };
 
-      Dio dio = Dio();
-      dio.options = BaseOptions(headers: headers);
+    Dio dio = Dio();
+    dio.options = BaseOptions(headers: headers);
 
-      final response = await dio.post(url, data: payload);
-
+    final response = await dio.post(url, data: payload);
+    if (response.statusCode == 200) {
+      File file = File('image.jpg');
+      file.writeAsBytesSync(response.data);
+      return file;
+    } else {
+      return null;
+    }
   }
 }
