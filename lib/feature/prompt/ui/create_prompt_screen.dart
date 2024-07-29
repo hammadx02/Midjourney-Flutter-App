d2c85c28acd569cce2401baf239d4988242f8079
hammadx02 <hammadaali02@gmail.com>
2024-07-29 21:59:59 +0500

    updated

--- lib/feature/prompt/ui/create_prompt_screen.dart
+++ lib/feature/prompt/ui/create_prompt_screen.dart
@@ -42,66 +42,74 @@ class _CreatePromptScreenState extends State<CreatePromptScreen> {
               return const Center(
                 child: CircularProgressIndicator(),
               );
-          }
 
-          return Container(
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Expanded(
-                  child: Container(
-                    width: double.maxFinite,
-                    color: Colors.deepPurple,
-                  ),
+            case const (PromptGenerateImageErrorState):
+              return const Center(
+                child: Text(
+                  "Something went wrong 🤖🔥",
                 ),
-                Container(
-                  height: 240,
-                  padding: const EdgeInsets.all(24),
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      const Text(
-                        "Enter your prompt",
-                        style: TextStyle(
-                          fontSize: 24,
-                          fontWeight: FontWeight.bold,
-                        ),
-                      ),
-                      const SizedBox(height: 20),
-                      TextField(
-                        controller: controller,
-                        cursorColor: Colors.deepPurple,
-                        decoration: InputDecoration(
-                          focusedBorder: OutlineInputBorder(
-                            borderSide:
-                                const BorderSide(color: Colors.deepPurple),
-                            borderRadius: BorderRadius.circular(12),
-                          ),
-                          border: OutlineInputBorder(
-                            borderRadius: BorderRadius.circular(12),
-                          ),
-                        ),
-                      ),
-                      const SizedBox(height: 20),
-                      SizedBox(
-                        height: 48,
+              );
+
+            case PromptGenerateImageSuccessState:
+              return Container(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Expanded(
+                      child: Container(
                         width: double.maxFinite,
-                        child: ElevatedButton.icon(
-                          style: ButtonStyle(
-                            backgroundColor:
-                                WidgetStateProperty.all(Colors.deepPurple),
+                        color: Colors.deepPurple,
+                      ),
+                    ),
+                    Container(
+                      height: 240,
+                      padding: const EdgeInsets.all(24),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          const Text(
+                            "Enter your prompt",
+                            style: TextStyle(
+                              fontSize: 24,
+                              fontWeight: FontWeight.bold,
+                            ),
                           ),
-                          onPressed: () {},
-                          icon: const Icon(Icons.generating_tokens),
-                          label: const Text("Generate"),
-                        ),
-                      )
-                    ],
-                  ),
-                )
-              ],
-            ),
-          );
+                          const SizedBox(height: 20),
+                          TextField(
+                            controller: controller,
+                            cursorColor: Colors.deepPurple,
+                            decoration: InputDecoration(
+                              focusedBorder: OutlineInputBorder(
+                                borderSide:
+                                    const BorderSide(color: Colors.deepPurple),
+                                borderRadius: BorderRadius.circular(12),
+                              ),
+                              border: OutlineInputBorder(
+                                borderRadius: BorderRadius.circular(12),
+                              ),
+                            ),
+                          ),
+                          const SizedBox(height: 20),
+                          SizedBox(
+                            height: 48,
+                            width: double.maxFinite,
+                            child: ElevatedButton.icon(
+                              style: ButtonStyle(
+                                backgroundColor:
+                                    WidgetStateProperty.all(Colors.deepPurple),
+                              ),
+                              onPressed: () {},
+                              icon: const Icon(Icons.generating_tokens),
+                              label: const Text("Generate"),
+                            ),
+                          )
+                        ],
+                      ),
+                    )
+                  ],
+                ),
+              );
+          }
         },
       ),
     );
