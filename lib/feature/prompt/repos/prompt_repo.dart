17335953c6b49f87e8b405b9271714eae800cde3
hammadx02 <hammadaali02@gmail.com>
2024-07-29 16:05:26 +0500

    updated

--- lib/feature/prompt/repos/prompt_repo.dart
+++ lib/feature/prompt/repos/prompt_repo.dart
@@ -1,5 +1,28 @@
 class PromptRepo {
   static generateImage(String prompt) async {
+
+    String url = 'https://api.vyro.ai/v1/imagine/api/generations';
+
     
+
   }
 }
+
+
+
+headers = {
+  'Authorization': 'Bearer vk-***************'
+}
+
+# Using None here allows us to treat the parameters as string
+payload = {
+  
+}
+
+response = requests.post(url, headers=headers, files=payload)
+
+if response.status_code == 200:  # if request is successful
+  with open('image.jpg', 'wb') as f:
+    f.write(response.content)
+else:
+  print("Error:", response.status_code)
