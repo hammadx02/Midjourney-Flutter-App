890a9438a5fb195772018639068f255472073173
hammadx02 <hammadaali02@gmail.com>
2024-07-29 16:07:51 +0500

    updated

--- lib/feature/prompt/repos/prompt_repo.dart
+++ lib/feature/prompt/repos/prompt_repo.dart
@@ -3,16 +3,25 @@ class PromptRepo {
 
     String url = 'https://api.vyro.ai/v1/imagine/api/generations';
 
-    
+    Map<String, dynamic> headers = {
+  'Authorization': 'Bearer vk-***************'
+};
+
+Map<String, dynamic> payload = {
+        'prompt': prompt,
+        'style_id': '122',
+        'aspect_ratio': '1:1',
+        'cfg': '5',
+        'seed': '1',
+        'high_res_results': '1'
+      };
 
   }
 }
 
 
 
-headers = {
-  'Authorization': 'Bearer vk-***************'
-}
+
 
 # Using None here allows us to treat the parameters as string
 payload = {
